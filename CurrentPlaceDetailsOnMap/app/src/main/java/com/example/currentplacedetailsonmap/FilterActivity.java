c35df7e1d3669debb498f8cc21a789dd375b7e31
Coaltergeist <coaltergeist@gmail.com>
2018-11-11 19:30:44 -0500

    Added info passed through intent

--- CurrentPlaceDetailsOnMap/app/src/main/java/com/example/currentplacedetailsonmap/FilterActivity.java
+++ CurrentPlaceDetailsOnMap/app/src/main/java/com/example/currentplacedetailsonmap/FilterActivity.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class FilterActivity extends AppCompatActivity {
 
+    private Spinner spinner1;
+    private Spinner spinner2;
+    private EditText mEdit;
     private Button confirm;
+    public String lotType;
+    public String specialty;
+    public String upperBound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +24,27 @@ public class FilterActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_filters);
 
+        spinner1 = (Spinner) findViewById(R.id.lotTypeDropDown);
+        spinner2 = (Spinner) findViewById(R.id.specialtyDropDown);
+        mEdit = (EditText) findViewById(R.id.upperBound);
         confirm = (Button) findViewById(R.id.confirm);
 
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goBack();
+                lotType = String.valueOf(spinner1);
+                specialty = String.valueOf(spinner2);
+                upperBound = String.valueOf(mEdit);
+                submit();
             }
         });
     }
 
-    public void goBack() {
+    public void submit() {
         Intent intent = new Intent(this, MapsActivityCurrentPlace.class);
+        intent.putExtra("lotType", lotType);
+        intent.putExtra("specialty", specialty);
+        intent.putExtra("upperBound", upperBound);
         startActivity(intent);
     }
 }
